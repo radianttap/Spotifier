0060805c72cc96ca9bb115813c4ddedfb82d8257
Aleksandar Vacic <av@codeaplus.com>
2019-02-11 23:59:38 +0100

    Added minimum code so the app can run

--- Spotifier/AppDelegate.swift
+++ Spotifier/AppDelegate.swift
@@ -19,13 +19,29 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication,
 					 willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
 	{
+		window = UIWindow(frame: UIScreen.main.bounds)
+
+		let vc = UIViewController()
+		let nc = UINavigationController(rootViewController: vc)
+		window?.rootViewController = nc
+
+		applyTheme()
+
 		return true
 	}
 
 	func application(_ application: UIApplication,
 					 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
 	{
+		window?.makeKeyAndVisible()
 		return true
 	}
 }
 
+
+private extension AppDelegate {
+	func applyTheme() {
+		guard let nc = window?.rootViewController as? UINavigationController else { return }
+		nc.navigationBar.barStyle = .blackTranslucent
+	}
+}
