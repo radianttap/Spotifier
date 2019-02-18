e51f5c1209fc1b369407b6824c95b7a017b61233
Aleksandar Vacic <av@codeaplus.com>
2019-02-18 17:53:39 +0100

    AppDelegate = keeper of shared instances.
    There is no other place to keep these instances in regular UIKit
    (apart from using them all as singletons).

--- Spotifier/AppDelegate.swift
+++ Spotifier/AppDelegate.swift
@@ -10,8 +10,21 @@ import UIKit
 
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-
 	var window: UIWindow?
+
+	//MARK:- Middleware
+
+	//	All the shared instances are kept here,
+	//	since AppDelegate is the only object that is always in memory
+	lazy var spotify: Spotify = Spotify()
+	lazy var dataManager: DataManager = DataManager(spotify: spotify)
+	lazy var accountManager: AccountManager = AccountManager(dataManager: dataManager)
+	lazy var contentManager: ContentManager = ContentManager(dataManager: dataManager)
+
+	//	Current value of all non-UI in use
+	var appDependency: AppDependency?
+
+
 
 
 	//	MARK:- Lifecycle
@@ -21,10 +34,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 	{
 		window = UIWindow(frame: UIScreen.main.bounds)
 
-		let vc = UIViewController()
-		let nc = UINavigationController(rootViewController: vc)
-		window?.rootViewController = nc
-
+		buildDependencies()
 		applyTheme()
 
 		return true
@@ -33,15 +43,36 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication,
 					 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
 	{
+		setupUI()
+
 		window?.makeKeyAndVisible()
 		return true
 	}
 }
 
 
+
+//MARK:- Private
+
 private extension AppDelegate {
+	///	Global theme (look & feel) for the app
 	func applyTheme() {
 		guard let nc = window?.rootViewController as? UINavigationController else { return }
 		nc.navigationBar.barStyle = .blackTranslucent
+	}
+
+	///	Build `appDependency` value as many times as you need
+	func buildDependencies() {
+		appDependency = AppDependency(spotify: spotify,
+									  dataManager: dataManager,
+									  accountManager: accountManager,
+									  contentManager: contentManager)
+	}
+
+	///	Built UI stack and supply the dependencies into it
+	func setupUI() {
+		let vc = UIViewController()
+		let nc = UINavigationController(rootViewController: vc)
+		window?.rootViewController = nc
 	}
 }
