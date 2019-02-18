b38efce45fef569da407ed8e67af45c92118ab7a
Aleksandar Vacic <av@codeaplus.com>
2019-02-18 14:21:23 +0100

    Declare SearchType values as enum

---
+++ Spotify/Spotify-Types.swift
@@ -0,0 +1,18 @@
+//
+//  Spotify-Types.swift
+//  Spotifier
+//
+//  Created by Aleksandar Vacić on 5/8/18.
+//  Copyright © 2018 Radiant Tap. All rights reserved.
+//
+
+import Foundation
+
+extension Spotify {
+
+	enum SearchType: String, CaseIterable {
+		case artist, album, track, playlist
+
+	}
+
+}
