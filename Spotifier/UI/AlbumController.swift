e1cf7eef24a67d12ecfbd3b8b77f45a8e8a93da6
Aleksandar Vacic <av@codeaplus.com>
2019-02-20 22:59:45 +0100

    Simple album VC content

--- Spotifier/UI/AlbumController.swift
+++ Spotifier/UI/AlbumController.swift
@@ -11,21 +11,50 @@ import UIKit
 final class AlbumController: UIViewController, StoryboardLoadable {
 	//	Data model
 
-	var album: Album?
+	var album: Album?{
+		didSet {
+			if !isViewLoaded { return }
+			populate()
+		}
+	}
 
 	//	UI
 
+	@IBOutlet private var photoView: UIImageView!
+	@IBOutlet private var photoContainerView: UIView!
+	@IBOutlet private var titleLabel: UILabel!
+	@IBOutlet private var yearLabel: UILabel!
+	@IBOutlet private var artistLabel: UILabel!
+}
 
-
-
-
-
+extension AlbumController {
 	// View lifecycle
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		title = album?.name
+		populate()
+	}
+}
+
+private extension AlbumController {
+	func cleanup() {
+		titleLabel.text = nil
+		yearLabel.text = nil
+		artistLabel.text = nil
+		photoView.image = nil
+	}
+
+	func populate() {
+		guard let album = album else {
+			cleanup()
+			return
+		}
+
+		titleLabel.text = album.name
+		yearLabel.text = album.releaseDate
+		artistLabel.text = album.artists.map{ $0.name }.joined(separator: ", ")
+		album.populateImageView(photoView, with: album.imageURL)
 	}
 }
 
