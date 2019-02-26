788890f4894f10320eafa038115a9be9aee370e8
Aleksandar Vacic <av@codeaplus.com>
2019-02-26 09:46:33 +0100

    Built Track model class

--- DataModel/Track.swift
+++ DataModel/Track.swift
@@ -9,5 +9,41 @@
 import Foundation
 
 final class Track: NSObject {
+	var trackId: String
+	var name: String
+	var durationInMilliseconds: TimeInterval
+	var trackNumber: Int
 
+	var discNumber: Int = 1
+	var isExplicit = false
+	var webURL: URL?			//	href
+	var popularity: Int = 0
+
+	var availableMarkets: Set<String> = []
+	var isPlayable = true		//	related to linkedFromTrack and TrackRestriction
+	var previewAudioURL: URL?
+
+	//	Relationships
+
+//	var linkedFromTrack: Track?
+//	var restrictions: [TrackRestriction] = []
+
+	var album: Album!	//	track must have an Album
+
+	var artists: [Artist] = []
+
+
+	//	Inits
+
+	private override init() {
+		fatalError("Use `init(id:name:)`")
+	}
+
+	init(id: String, name: String, duration: TimeInterval, trackNumber: Int) {
+		self.trackId = id
+		self.name = name
+		self.durationInMilliseconds = duration
+		self.trackNumber = trackNumber
+		super.init()
+	}
 }
