a547cd9db808759933867edc7314e4f7731a8a1f
Aleksandar Vacić <av@radianttap.com>
2019-03-23 15:47:11 +0100

    Skeleton PlayManager class

---
+++ Spotifier/Middleware/PlayManager.swift
@@ -0,0 +1,28 @@
+//
+//  PlayManager.swift
+//  Spotifier
+//
+//  Created by Aleksandar Vacić on 3/23/19.
+//  Copyright © 2019 Radiant Tap. All rights reserved.
+//
+
+import Foundation
+
+final class PlayManager {
+
+	private(set) var tracks: [Track] = []
+}
+
+extension PlayManager {
+	func queueTrack(_ track: Track) {
+
+	}
+
+	func playTrack(_ track: Track) {
+
+	}
+
+	func removeTrack(_ track: Track) {
+
+	}
+}
