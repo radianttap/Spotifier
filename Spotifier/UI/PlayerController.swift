d580afc0b55c9418d641dd1fc19c66488fba25b7
Aleksandar Vacić <av@radianttap.com>
2019-03-23 16:35:04 +0100

    Simple playlist display

--- Spotifier/UI/PlayerController.swift
+++ Spotifier/UI/PlayerController.swift
@@ -2,11 +2,58 @@
 //  PlayerController.swift
 //  Spotifier
 //
-//  Created by Aleksandar Vacić on 5/8/18.
-//  Copyright © 2018 Radiant Tap. All rights reserved.
+//  Created by Aleksandar Vacić on 3/23/19.
+//  Copyright © 2019 Radiant Tap. All rights reserved.
 //
 
 import UIKit
 
-final class PlayerController: UIViewController {
+final class PlayerController: UICollectionViewController {
+	init() {
+		let l = TrackListLayout()
+		super.init(collectionViewLayout: l)
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	//	Data source
+
+	var tracks: [Track] = []
+}
+
+extension PlayerController {
+	// View lifecycle
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		setupCollectionView()
+		populate()
+	}
+}
+
+private extension PlayerController {
+	func populate() {
+		collectionView.reloadData()
+	}
+
+	func setupCollectionView() {
+		collectionView.register(TrackCell.self)
+	}
+}
+
+extension PlayerController {
+	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return tracks.count
+	}
+
+	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let track = tracks[indexPath.item]
+
+		let cell: TrackCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
+		cell.populate(with: track, context: .playlist)
+		return cell
+	}
 }
