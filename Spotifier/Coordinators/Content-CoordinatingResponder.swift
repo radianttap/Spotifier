c988ec5c4999a6dbfea0040d62655fa6f89b9337
Aleksandar Vacić <av@radianttap.com>
2019-04-06 13:13:48 +0200

    Re-formatted to make the params more obvious

--- Spotifier/Coordinators/Content-CoordinatingResponder.swift
+++ Spotifier/Coordinators/Content-CoordinatingResponder.swift
@@ -10,15 +10,25 @@ import UIKit
 import Coordinator
 
 extension UIResponder {
-	@objc func contentDisplayAlbum(_ album: Album, onQueue queue: OperationQueue? = .main, sender: Any?) {
+	@objc func contentDisplayAlbum(_ album: Album,
+								   onQueue queue: OperationQueue? = .main,
+								   sender: Any?)
+	{
 		coordinatingResponder?.contentDisplayAlbum(album, onQueue: queue, sender: sender)
 	}
 
-	@objc func contentDisplayArtist(_ artist: Artist, onQueue queue: OperationQueue? = .main, sender: Any?) {
+	@objc func contentDisplayArtist(_ artist: Artist,
+									onQueue queue: OperationQueue? = .main,
+									sender: Any?)
+	{
 		coordinatingResponder?.contentDisplayArtist(artist, onQueue: queue, sender: sender)
 	}
 
-	@objc func contentSearch(for term: String, onQueue queue: OperationQueue? = nil, sender: Any?, callback: @escaping (String, [SearchResultBox], Error?) -> Void) {
+	@objc func contentSearch(for term: String,
+							 onQueue queue: OperationQueue? = nil,
+							 sender: Any?,
+							 callback: @escaping (String, [SearchResultBox], Error?) -> Void)
+	{
 		coordinatingResponder?.contentSearch(for: term, onQueue: queue, sender: sender, callback: callback)
 	}
 }
