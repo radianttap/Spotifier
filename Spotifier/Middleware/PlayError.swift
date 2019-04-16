5533278f82a676fcfb082a8f7109b041303d9c47
Aleksandar Vacić <av@radianttap.com>
2019-04-16 19:55:00 +0200

    Each middleware module should define its own Errors, thus adding one for PlayManager

---
+++ Spotifier/Middleware/PlayError.swift
@@ -0,0 +1,13 @@
+//
+//  PlayError.swift
+//  Spotifier
+//
+//  Created by Aleksandar Vacić on 4/16/19.
+//  Copyright © 2019 Radiant Tap. All rights reserved.
+//
+
+import Foundation
+
+enum PlayError {
+	case trackAlreadyAdded
+}
