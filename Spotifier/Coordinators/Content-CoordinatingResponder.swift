f8fb12ca9ca2de0e6e19d3a3906187440c155ac0
Aleksandar Vacić <av@radianttap.com>
2019-09-05 18:17:27 +0200

    DataSource objects live in the UI space, but their entire functionality is dependent on the middleware/data layers. So they should have a way to directly contact their true dependency.
    Thus remove this coordinatingResponder method and re-factor the data flow.

--- Spotifier/Coordinators/Content-CoordinatingResponder.swift
+++ Spotifier/Coordinators/Content-CoordinatingResponder.swift
@@ -23,12 +23,4 @@ extension UIResponder {
 	{
 		coordinatingResponder?.contentDisplayArtist(artist, onQueue: queue, sender: sender)
 	}
-
-	@objc func contentSearch(for term: String,
-							 onQueue queue: OperationQueue? = nil,
-							 sender: Any?,
-							 callback: @escaping (String, [SearchResultBox], Error?) -> Void)
-	{
-		coordinatingResponder?.contentSearch(for: term, onQueue: queue, sender: sender, callback: callback)
-	}
 }
